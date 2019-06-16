40303cdb5565101897ffb6017d64bc25200e9acd
Kaja <catcalledkaja@gmail.com>
2019-06-16 15:58:40 +0100

    Finished FizzBuzz with working test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,12 @@
 def fizzbuzz(number)
-'fizz'
+  if number % 3 == 0 && number % 5 != 0
+    'fizz'
+  elsif number % 5 == 0 && number % 3!= 0
+    'buzz'
+  #if number == 15
+   elsif number % 3 == 0 && number % 5 == 0
+    'fizzbuzz'
+  else
+    number
+  end
 end
